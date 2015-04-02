b78b4602b6439ffd04b3f0fa9655e99e59f2c041
bayuhafiz <bayu@colorblindlabs.com>
2015-04-02 11:14:53 +0700

    updated: user seeding

--- app/seeding/user.js
+++ app/seeding/user.js
@@ -1,22 +1,43 @@
 var User = require('../models/user');
 
-// create the user
+// create user #1
 var newUser = new User();
 
-newUser.firstName = 'Jobsy';
+newUser.firstName = 'Rendy';
 newUser.lastName = 'Admin';
 newUser.companyName = 'Colorblind Labs';
-newUser.email = 'bayu@colorblindlabs.com';
-newUser.password = newUser.generateHash('qwpo');
-newUser.actStatus = 'inactive';
-newUser.initLogin = true;
-newUser.actToken = '123';
+newUser.email = 'rendy@colorblindlabs.com';
+newUser.password = newUser.generateHash('hello123');
+newUser.actStatus = 'activated';
+newUser.initLogin = false;
 
 User.findOne({
-    email: 'bayu@colorblindlabs.com'
+    email: 'rendy@colorblindlabs.com'
 }, function(err, existingUser) {
     if (!existingUser) {
         newUser.save(function(err) {
+            if (err)
+                return done(err);
+        });
+    }
+});
+
+// create user #2
+var newUser2 = new User();
+
+newUser2.firstName = 'Pras';
+newUser2.lastName = 'Admin';
+newUser2.companyName = 'Colorblind Labs';
+newUser2.email = 'pras@colorblindlabs.com';
+newUser2.password = newUser2.generateHash('hello123');
+newUser2.actStatus = 'activated';
+newUser2.initLogin = false;
+
+User.findOne({
+    email: 'pras@colorblindlabs.com'
+}, function(err, existingUser) {
+    if (!existingUser) {
+        newUser2.save(function(err) {
             if (err)
                 return done(err);
         });
