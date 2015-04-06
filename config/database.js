8bbd8d8015af5e8610b95fecde33ac760a98c1f3
bayuhafiz <bayu@colorblindlabs.com>
2015-04-06 17:32:06 +0700

    pulling

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
 // config/database.js
 module.exports = {
-
-	'url' : 'mongodb://localhost:27017/jobsy' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
+	//'url' : 'mongodb://localhost:27017/jobsy'
+	'url' : 'mongodb://colorblindlabs:hello123#@ds059821.mongolab.com:59821/jobsy'
 };
