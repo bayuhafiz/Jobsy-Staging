04dd05b073fd6f696eb3ab24d1e916dd5f4721cc
bayuhafiz <bayu@colorblindlabs.com>
2015-04-21 19:42:15 +0700

    updated: mailgun settings

--- config/secret.js
+++ config/secret.js
@@ -2,8 +2,8 @@ module.exports = {
 
 	// Mailgun credentials
     mailgun: {
-        user: process.env.MAILGUN_USER || 'postmaster@mg.colorblindlabs.com',
-        password: process.env.MAILGUN_PASSWORD || '2110b5de8bc0901a30477989ad9732b9'
+        user: process.env.MAILGUN_USER || 'postmaster@beta.jobsy.io',
+        password: process.env.MAILGUN_PASSWORD || '68d197dd36d0584ce2cde6a90b69b93a'
     }
 
 }
