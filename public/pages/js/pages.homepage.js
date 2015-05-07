c6a53bdb6b0835fd195d50113e997384b05ab1d9
indralesmana10 <indralesmana.mmb@gmail.com>
2015-05-07 12:56:35 +0700

    update jobsy

--- public/pages/js/pages.homepage.js
+++ public/pages/js/pages.homepage.js
@@ -750,7 +750,7 @@
                 if (searchBox.val() == '') {
                     $(".searchJob").val('').attr('placeholder', 'Search here..');
                 }
-                container.hide();
+                container.slideUp('fast');
             }
         });
         ///////////// END OF SEARCH INPUT HANDLER //////////////
