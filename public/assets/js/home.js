dd8c2270c810d6a782ac739dbb4eaeaf332c6879
indralesmana10 <indralesmana.mmb@gmail.com>
2015-05-19 13:00:34 +0700

    update jobsy

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -738,15 +738,24 @@
                     'right': '-240px'
                 });
 
-            $(this).find('.datetime').animate({
+
+            if($(this).children('.apply-btn').children('p').text() == 'Logout to apply'){
+                $(this).find('.datetime').animate({
+                    'right': '190px'
+                }).end().siblings()
+                .find('.datetime').animate({
+                    'right': '20px'
+                });
+            } else {
+                $(this).find('.datetime').animate({
                     'right': '160px'
                 }).end().siblings()
                 .find('.datetime').animate({
                     'right': '20px'
                 });
+            }
 
-
-
+            
             $('.details-list-box').css({
                 'position': 'relative'
             })
