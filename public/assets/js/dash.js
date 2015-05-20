4e2487c5768a922f22246dd1b136d15a9597777c
indralesmana10 <indralesmana.mmb@gmail.com>
2015-05-20 11:43:59 +0700

    update jobsy

--- public/assets/js/dash.js
+++ public/assets/js/dash.js
@@ -269,6 +269,18 @@
 
     $(document).ready(function() {
 
+
+        $('#btn-cropper-done,#btn-cropper-close').click(function () {
+            $('#PostNewJob').css({'overflow-x':'hidden','overflow-y':'auto','z-index':'1041'});
+            $('body').css('overflow','hidden');
+        });
+
+        $('#PostNewJob .close').click(function () {
+            $('body').css('overflow','auto');
+        });
+
+        
+
         //////// Image processing /////////
         var options = {
             thumbBox: '.thumbBox',
