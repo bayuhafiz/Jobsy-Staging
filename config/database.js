dea1a8f61d8c343967c6c0e3aec8007be3d159c1
bayuhafiz <bayu@colorblindlabs.com>
2015-06-08 12:50:16 +0700

    fixed: email template

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 // config/database.js
 module.exports = {
-	//'url' : 'mongodb://localhost:27017/jobsy'
-	'url' : 'mongodb://bayu@colorblindlabs.com:Hello123#@ds043962.mongolab.com:43962/jobsy_db'
+	'url' : 'mongodb://localhost:27017/jobsy'
+	//'url' : 'mongodb://bayu@colorblindlabs.com:Hello123#@ds043962.mongolab.com:43962/jobsy_db'
 };
