07cb3497c89e3134649d2906437b11d25e565da8
bayuhafiz <bayu@colorblindlabs.com>
2015-06-08 12:09:17 +0700

    updated: database creds

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 // config/database.js
 module.exports = {
-	'url' : 'mongodb://localhost:27017/jobsy'
+	//'url' : 'mongodb://localhost:27017/jobsy'
+	'url' : 'mongodb://<dbuser>:<dbpassword>@ds043962.mongolab.com:43962/jobsy_db'
 };
